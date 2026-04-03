e0beaec1d3215ef07ac493040aa0b32d863a7504
Vũ Tuấn Hải <43202025+vutuanhai237@users.noreply.github.com>
2026-04-03 21:33:30 +0700

    Add slide

---
+++ IT002-OOP/C++ Demos/buildWithHeaderFile/src/hello.cpp
@@ -0,0 +1,7 @@
+using namespace std;
+#include <iostream>
+#include "../include/hello.h"
+
+void HelloWorld::greet() {
+    std::cout << "C++ 03/03/2026" << std::endl;
+}
---
+++ IT002-OOP/C++ Demos/buildWithHeaderFile/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+#include "../include/hello.h"
+
+int main() {
+    HelloWorld hw;
+    hw.greet();
+    return 0;
+}
